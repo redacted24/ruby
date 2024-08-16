b51985179867e98819f5b08501986056623c7dc4
Qiuyu Huang <huangq4@outlook.com>
2024-08-15 21:22:15 -0400

    done: chap 9 debugging with ruby

--- ruby-exercises/ruby_basics/9_debugging/exercises/debugging_exercises.rb
+++ ruby-exercises/ruby_basics/9_debugging/exercises/debugging_exercises.rb
@@ -6,41 +6,47 @@
 # To the top, the first line, where the error occured, and ONLY THEN fix the error
 
 def decrement_smallest_value(nested_array)
-  smallest_value = nested_array.flatten.max
-  nested_array.each do |array|
-    array.each do |current_value|
-      if smallest_value > current_value
-        smallest_value = current_valu
-      end
+    smallest_value = nested_array.flatten.max
+    nested_array.each do |array|
+        array.each do |current_value|
+            if smallest_value > current_value
+                smallest_value = current_value
+            end
+        end
     end
-  end
-  smallest_value -= 1
+    smallest_value -= 1
 end
+
+# done
 
 # use the stack trace to debug the following method
 # Don't look at this method prior to running the test
 # Run rspec, let the test fail, and go through the stack trace again
 def increment_greatest_value(nested_array)
-  greatest_value = nested_array.flatten.min
-  nested_array.each do |array|
-    array.each do |current_value|
-      if greatest_value < nil
-        greatest_value = current_value
-      end
+    greatest_value = nested_array.flatten.min
+    nested_array.each do |array|
+        array.each do |current_value|
+            if greatest_value < current_value
+                greatest_value = current_value
+            end
+        end
     end
-  end
-  greatest_value += 1
+    greatest_value += 1
 end
+
+# done
 
 # This next exercise might look familiar
 # Use p and puts in order to find what's wrong with our method
 
 def isogram?(string)
-  original_length = string.length
-  string_array = string.downcase.split
-  unique_length = string_array.uniq.length
-  original_length == unique_length
+    original_length = string.length
+    string_array = string.downcase.split('')
+    unique_length = string_array.uniq.length
+    original_length == unique_length
 end
+
+# done
 
 # Can you guess what's next?
 # That's right! The final exercise from the lesson, which we'll debug with pry-byebug
@@ -49,8 +55,10 @@ end
 # Next insert plenty of breakpoints, and see if you can tell where things break
 # Once you find the error, fix it and get the test to pass
 
+require 'pry-byebug'
+
 def yell_greeting(string)
-  name = string
-  name = name.downcase
-  greeting = "WASSAP, #{name}!"
+    name = string
+    name = name.upcase
+    greeting = "WASSAP, #{name}!"
 end
